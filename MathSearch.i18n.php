362d8ffa4e2a1d92a79a505188542bc9662955f4
raymond <raimond.spekking@gmail.com>
2012-10-28 13:02:16 +0100

    Kill trailing spaces, breaks Translatewiki.net scripts
    Change-Id: I9542e781df3f251ff01873b190c970a10f8d8358

--- MathSearch.i18n.php
+++ MathSearch.i18n.php
@@ -6,14 +6,14 @@
  * @ingroup Extensions
  */
 $messages = array();
- 
+
 /** English
  * @author Moritz Schubotz
  */
 $messages['en'] = array(
         'mathsearch-desc' => 'Integrates the [http://search.mathweb.org/about.html MathWeb Search] engine',
 );
- 
+
 /** Message documentation
  * @author Moritz Schubotz
  */
