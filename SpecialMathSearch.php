47816d495320bf9f9fe8d57a342b8c351c2d0e16
physikerwelt <wiki@physikerwelt.de>
2014-01-21 22:35:23 +0000

    switch to cirrus search
    Change-Id: Ib6c0c8f238d343edbc4ea87d5b9694cd07c9f0ec

--- SpecialMathSearch.php
+++ SpecialMathSearch.php
@@ -28,18 +28,18 @@ class SpecialMathSearch extends SpecialPage {
 	 *
 	 */
 	function __construct() {
-		parent::__construct('MathSearch');
+		parent::__construct( 'MathSearch' );
 	}
 
 	/**
 	 *
-	 * @return \LuceneSearch|boolean
+	 * @return \CirrusSearch|boolean
 	 */
-	public static function getLucene() {
-		if (class_exists("LuceneSearch")) {
-			return new LuceneSearch();
+	public static function getCirrusSearch() {
+		if ( class_exists( 'CirrusSearch' ) ) {
+			return new CirrusSearch();
 		} else {
-			wfDebugLog("MathSearch", "Text search not possible. Class LuceneSearch is missing.");
+			wfDebugLog( 'MathSearch', 'Text search not possible. Class CirrusSearch is missing.' );
 			return false;
 		}
 	}
@@ -47,17 +47,17 @@ class SpecialMathSearch extends SpecialPage {
 	/**
 	 * The main function
 	 */
-	public function execute($par) {
+	public function execute( $par ) {
 		$request = $this->getRequest();
 		$this->setHeaders();
-		$this->mathpattern = $request->getText('mathpattern');
-		$this->textpattern = $request->getText('textpattern', '');
-		$isEncoded = $request->getBool('isEncoded', false);
-		if ($isEncoded) {
-			$this->mathpattern = htmlspecialchars_decode($this->mathpattern);
+		$this->mathpattern = $request->getText( 'mathpattern' );
+		$this->textpattern = $request->getText( 'textpattern', '' );
+		$isEncoded = $request->getBool( 'isEncoded', false );
+		if ( $isEncoded ) {
+			$this->mathpattern = htmlspecialchars_decode( $this->mathpattern );
 		}
 		$this->searchForm();
-		if ($this->mathpattern || $this->textpattern) {
+		if ( $this->mathpattern || $this->textpattern ) {
 			$this->performSearch();
 		}
 	}
@@ -81,14 +81,14 @@ class SpecialMathSearch extends SpecialPage {
 				'default' => $this->textpattern,
 			)
 		);
-		if (!self::getLucene()) {
+		if ( !self::getCirrusSearch() ) {
 			$formDescriptor['textpattern']['disabled'] = true;
-			$formDescriptor['textpattern']['help'] = 'LuceneSearch not found. Text search <b>disabled</b>!<br/> For details see <a href=\"http://www.mediawiki.org/wiki/Extension:MWSearch\">MWSearch</a>.';
+			$formDescriptor['textpattern']['help'] = 'CirrusSearch not found. Text search <b>disabled</b>!<br/> For details see <a href=\"http://www.mediawiki.org/wiki/Extension:MWSearch\">MWSearch</a>.';
 		}
-		$htmlForm = new HTMLForm($formDescriptor, $this->getContext()); # We build the HTMLForm object
-		$htmlForm->setSubmitText('Search');
-		$htmlForm->setSubmitCallback(array(get_class($this), 'processInput'));
-		$htmlForm->setHeaderText("<h2>Input</h2>");
+		$htmlForm = new HTMLForm( $formDescriptor, $this->getContext() ); # We build the HTMLForm object
+		$htmlForm->setSubmitText( 'Search' );
+		$htmlForm->setSubmitCallback( array( get_class( $this ), 'processInput' ) );
+		$htmlForm->setHeaderText( "<h2>Input</h2>" );
 		$htmlForm->show(); # Displaying the form
 	}
 
@@ -96,7 +96,7 @@ class SpecialMathSearch extends SpecialPage {
 	 * Processes the submitted Form input
 	 * @param array $formData
 	 */
-	public static function processInput($formData) {
+	public static function processInput( $formData ) {
 		$instance = new SpecialMathSearch();
 		$instance->mathpattern = $formData['mathpattern'];
 		$instance->textpattern = $formData['textpattern'];
@@ -108,56 +108,56 @@ class SpecialMathSearch extends SpecialPage {
 	 * @param unknown $pageID
 	 * @return boolean
 	 */
-	function DisplayMath($pageID) {
+	function DisplayMath( $pageID ) {
 		global $wgMathDebug;
 		$out = $this->getOutput();
 		$page = $this->mathResults[(string) $pageID];
-		$dbr = wfGetDB(DB_SLAVE);
-		$article = Article::newFromId($pageID);
+		$dbr = wfGetDB( DB_SLAVE );
+		$article = Article::newFromId( $pageID );
 		$pagename = (string) $article->getTitle();
-		wfDebugLog("MathSearch", "Processing results for $pagename");
-		foreach ($page as $anchorID => $answ) {
+		wfDebugLog( "MathSearch", "Processing results for $pagename" );
+		foreach ( $page as $anchorID => $answ ) {
 			$res = $dbr->selectRow(
-					array('mathindex', 'math'), array('math_mathml', 'mathindex_page_id', 'mathindex_anchor',
-				'mathindex_inputhash', 'math_inputhash'), 'mathindex_page_id = "' . $pageID
+					array( 'mathindex', 'math' ), array( 'math_mathml', 'mathindex_page_id', 'mathindex_anchor',
+				'mathindex_inputhash', 'math_inputhash' ), 'mathindex_page_id = "' . $pageID
 					. '" AND mathindex_anchor= "' . $anchorID
 					. '" AND mathindex_inputhash = math_inputhash'
 			);
-			if ($res) {
-				$mml = utf8_decode($res->math_mathml);
-				$out->addWikiText("====[[$pagename#math$anchorID|Eq: $anchorID (Result " . $this->resultID++ . ")]]====", false);
-				//$out->addHtml(MathLaTeXML::embedMathML($mml));
-				$out->addHtml("<br />");
+			if ( $res ) {
+				$mml = utf8_decode( $res->math_mathml );
+				$out->addWikiText( "====[[$pagename#math$anchorID|Eq: $anchorID (Result " . $this->resultID++ . ")]]====", false );
+				// $out->addHtml(MathLaTeXML::embedMathML($mml));
+				$out->addHtml( "<br />" );
 				$xpath = $answ[0]['xpath'];
-				$xmml = new SimpleXMLElement($res->math_mathml);
-				//TODO: Remove hack and report to Prode that he fixes that
-				//$xmml->registerXPathNamespace('m', 'http://www.w3.org/1998/Math/MathML');
-				$xpath = str_replace('/m:semantics/m:annotation-xml[@encoding="MathML-Content"]','',$xpath);
-				if(!$wgMathDebug){
-					$out->addWikiText("xPATH:".$xpath);
-					$out->addWikiText('MATHML:<source lang="xml">'.$xmml->asXML().'</source>');
+				$xmml = new SimpleXMLElement( $res->math_mathml );
+				// TODO: Remove hack and report to Prode that he fixes that
+				// $xmml->registerXPathNamespace('m', 'http://www.w3.org/1998/Math/MathML');
+				$xpath = str_replace( '/m:semantics/m:annotation-xml[@encoding="MathML-Content"]', '', $xpath );
+				if ( !$wgMathDebug ) {
+					$out->addWikiText( "xPATH:" . $xpath );
+					$out->addWikiText( 'MATHML:<source lang="xml">' . $xmml->asXML() . '</source>' );
 				}
 				$hit = $xmml->xpath( $xpath );
 				while ( list( , $node ) = each( $hit ) ) {
-					//$out->addHtml(var_export($node["xref"][0],true));
+					// $out->addHtml(var_export($node["xref"][0],true));
 					$dom = new DOMDocument;
-					$dom->loadXML($mml);
-					$domRes=$dom->getElementById($node["xref"][0]);
-					if ($domRes){
-						$domRes->setAttribute('mathcolor', '#cc0000');
-						$out->addHtml( $domRes->ownerDocument->saveXML());
+					$dom->loadXML( $mml );
+					$domRes = $dom->getElementById( $node["xref"][0] );
+					if ( $domRes ) {
+						$domRes->setAttribute( 'mathcolor', '#cc0000' );
+						$out->addHtml( $domRes->ownerDocument->saveXML() );
 					} else {
 						$renderer = new MathMathML();
-						$renderer->setMathml($mml);
-						$out->addHtml($renderer->getHtmlOutput());
+						$renderer->setMathml( $mml );
+						$out->addHtml( $renderer->getHtmlOutput() );
 					}
 				}
 
 
-				wfDebugLog("MathSearch", "PositionInfo:" . var_export($this->mathResults[$pageID][$anchorID], true));
+				wfDebugLog( "MathSearch", "PositionInfo:" . var_export( $this->mathResults[$pageID][$anchorID], true ) );
 			}
 			else
-				wfDebugLog("MathSearch", "Failure: Could not get entry $anchorID for page $pagename (id $pageID) :" . var_export($this->mathResults, true));
+				wfDebugLog( "MathSearch", "Failure: Could not get entry $anchorID for page $pagename (id $pageID) :" . var_export( $this->mathResults, true ) );
 		}
 		// var_dump($answ);
 // 		$xansw=new SimpleXMLElement($answ);
@@ -174,95 +174,96 @@ class SpecialMathSearch extends SpecialPage {
 	public function performSearch() {
 		global $wgMathDebug;
 		$out = $this->getOutput();
-		$time_start = microtime(true);
-		$out->addWikiText('==Results==');
-		$out->addWikiText('You serached for the LaTeX pattern "' . $this->mathpattern . '" and the text pattern "' . $this->textpattern . '".');
-		if ($this->mathpattern) {
-			if ($this->render()) {
-				$out->addWikiText("Your mathpattern was suceessfully rendered!");
-				if ($wgMathDebug) {
-					$out->addWikiText(" <source lang=\"xml\">" . $this->mathmlquery . "</source>");
+		$time_start = microtime( true );
+		$out->addWikiText( '==Results==' );
+		$out->addWikiText( 'You serached for the LaTeX pattern "' . $this->mathpattern . '" and the text pattern "' . $this->textpattern . '".' );
+		if ( $this->mathpattern ) {
+			if ( $this->render() ) {
+				$out->addWikiText( "Your mathpattern was suceessfully rendered!" );
+				if ( $wgMathDebug ) {
+					$out->addWikiText( " <source lang=\"xml\">" . $this->mathmlquery . "</source>" );
 				}
-				if ($this->postQuery()) {
-					$out->addWikiText("Your mathquery was sucessfully submitted and " . $this->numMathResults . " hits were obtained.");
+				if ( $this->postQuery() ) {
+					$out->addWikiText( "Your mathquery was sucessfully submitted and " . $this->numMathResults . " hits were obtained." );
 				} else {
-					$out->addWikiText("Failed to post query.");
+					$out->addWikiText( "Failed to post query." );
 				}
-				$time_end = microtime(true);
+				$time_end = microtime( true );
 				$time = $time_end - $time_start;
-				wfDebugLog("MathSearch", "math searched in $time seconds");
+				wfDebugLog( "MathSearch", "math searched in $time seconds" );
 			} else {
-				$out->addWikiText("Your query could not be renderded see the DebugLog for details.");
+				$out->addWikiText( "Your query could not be renderded see the DebugLog for details." );
 			}
 			// $out->addHTML(var_export($this->mathResults, true));
 		} else {
-			$out->addWikiText('The math-pattern is empty. No math search has been performed.');
-			$out->addWikiText("To view the text results click [{{canonicalurl:search|search=$this->textpattern}} Text-only search].");
+			$out->addWikiText( 'The math-pattern is empty. No math search has been performed.' );
+			$out->addWikiText( "To view the text results click [{{canonicalurl:search|search=$this->textpattern}} Text-only search]." );
 		}
 
-		if ($this->textpattern == "") {
+		if ( $this->textpattern == "" ) {
 			$mathout = "";
-			if ($this->mathResults) {
-				foreach ($this->mathResults as $pageID => $page) {
-					$article = Article::newFromId($pageID);
-					if ($article) {
+			if ( $this->mathResults ) {
+				foreach ( $this->mathResults as $pageID => $page ) {
+					$article = Article::newFromId( $pageID );
+					if ( $article ) {
 						$pagename = (string) $article->getTitle();
-						$out->addWikiText("==[[$pagename]]==");
-						$this->DisplayMath($pageID);
+						$out->addWikiText( "==[[$pagename]]==" );
+						$this->DisplayMath( $pageID );
 					}
 					else
-						$out->addWikiText("Error with Page (ID=$pageID) update math index.\n");
+						$out->addWikiText( "Error with Page (ID=$pageID) update math index.\n" );
 				}
 			}
 		}
-		$ls = self::getLucene();
-		if ($ls) {
+		$ls = self::getCirrusSearch();
+		if ( $ls ) {
 			$ls->limit = 1000000;
-			if ($this->textpattern) {
-				$sres = $ls->searchText($textpattern);
-				if ($sres && $sres->hasResults()) {
-					$out->addWikiText("You searched for the text '$textpattern' and the TeX-Pattern '$pattern'.");
-					$out->addWikiText("The text search results in [{{canonicalurl:search|search=$textpattern}} " .
+			if ( $this->textpattern ) {
+				$textpattern = $this->textpattern;
+				$sres = $ls->searchText( $textpattern );
+				if ( $sres && $sres->hasResults() ) {
+					$out->addWikiText( "You searched for the text '$textpattern' and the TeX-Pattern '$pattern'." );
+					$out->addWikiText( "The text search results in [{{canonicalurl:search|search=$textpattern}} " .
 							$sres->getTotalHits()
 							. "] hits and the math pattern matched $this->numMathResults times on [{{canonicalurl:{{FULLPAGENAMEE}}|pattern=$pattern}} " .
-							sizeof($this->relevantMathMap) .
-							"] pages.");
+							sizeof( $this->relevantMathMap ) .
+							"] pages." );
 					//// var_dump($sres);
-					wfDebugLog('mathsearch', 'BOF');
+					wfDebugLog( 'mathsearch', 'BOF' );
 					// $out->addWikiText(var_export($this->relevantMathMap,true));
 					$pageList = "";
-					while ($tres = $sres->next()) {
+					while ( $tres = $sres->next() ) {
 						$pageID = $tres->getTitle()->getArticleID();
 						// $out->addWikiText($pageID);
 
-						if (isset($this->relevantMathMap[$pageID])) {
-							$out->addWikiText("[[" . $tres->getTitle() . "]]");
-							$out->addHtml($tres->getTextSnippet($textpattern));
+						if ( isset( $this->relevantMathMap[$pageID] ) ) {
+							$out->addWikiText( "[[" . $tres->getTitle() . "]]" );
+							$out->addHtml( $tres->getTextSnippet( $textpattern ) );
 							$pageList .= "OR [[" . $pageID . "]]";
 							// $out->addHtml($this->showHit($tres),$textpattern);
-							$this->DisplayMath($pageID);
+							$this->DisplayMath( $pageID );
 						} /* else {
 						  $out->addWikiText(":NO MATH");
 						  }// */
 					} // $tres->mHighlightTitle)}
 
-					wfDebugLog('mathsearch', 'EOF');
-					wfDebugLog('mathsearch', var_export($this->mathResults, true));
+					wfDebugLog( 'mathsearch', 'EOF' );
+					wfDebugLog( 'mathsearch', var_export( $this->mathResults, true ) );
 				}
 			}
 		}
 		// $out->addHtml(htmlspecialchars( $pattern) );
-		$out->addWikiText("<math> $this->mathpattern </math>");
+		$out->addWikiText( "<math> $this->mathpattern </math>" );
 		// dbw = wfGetDB( DB_MASTER );$dbw->encodeBlob(pack( 'H32'
 		// $inputhash= $dbw->encodeBlob(pack( 'H32',md5($pattern) );
 		// $out->addWikiText("$inputhash");
-		$dbr = wfGetDB(DB_SLAVE);
-		$inputhash = $dbr->encodeBlob(pack('H32', md5($this->mathpattern)));
+		$dbr = wfGetDB( DB_SLAVE );
+		$inputhash = $dbr->encodeBlob( pack( 'H32', md5( $this->mathpattern ) ) );
 		$rpage = $dbr->select(
-				'mathindex', array('mathindex_page_id', 'mathindex_anchor', 'mathindex_timestamp'), array('mathindex_inputhash' => $inputhash)
+				'mathindex', array( 'mathindex_page_id', 'mathindex_anchor', 'mathindex_timestamp' ), array( 'mathindex_inputhash' => $inputhash )
 		);
-		foreach ($rpage as $row)
-			wfDebugLog("MathSearch", var_export($row, true));
+		foreach ( $rpage as $row )
+			wfDebugLog( "MathSearch", var_export( $row, true ) );
 	}
 
 
@@ -271,12 +272,12 @@ class SpecialMathSearch extends SpecialPage {
 	 * @return boolean
 	 */
 	function render() {
-		$renderer = new MathLaTeXMLML($this->mathpattern);
-		$renderer->setLaTeXMLSettings('profile=mwsquery');
-		$renderer->setAllowedRootElments(array('query'));
-		$renderer->render(true);
+		$renderer = new MathLaTeXMLML( $this->mathpattern );
+		$renderer->setLaTeXMLSettings( 'profile=mwsquery' );
+		$renderer->setAllowedRootElments( array( 'query' ) );
+		$renderer->render( true );
 		$this->mathmlquery = $renderer->getMathml();
-		if (strlen($this->mathmlquery) == 0) {
+		if ( strlen( $this->mathmlquery ) == 0 ) {
 			return false;
 		} else {
 			return true;
@@ -291,51 +292,51 @@ class SpecialMathSearch extends SpecialPage {
 		global $wgMWSUrl, $wgMathDebug;
 
 		$numProcess = 30000;
-		$tmp = str_replace("answsize=\"30\"", "answsize=\"$numProcess\" totalreq=\"yes\"", $this->mathmlquery);
-		$mwsExpr = str_replace("m:", "", $tmp);
-		wfDebugLog('mathsearch', 'MWS query:' . $mwsExpr);
-		$res = Http::post($wgMWSUrl, array("postData" => $mwsExpr, "timeout" => 60));
-		if ($res == false) {
-			if (function_exists('curl_init')) {
+		$tmp = str_replace( "answsize=\"30\"", "answsize=\"$numProcess\" totalreq=\"yes\"", $this->mathmlquery );
+		$mwsExpr = str_replace( "m:", "", $tmp );
+		wfDebugLog( 'mathsearch', 'MWS query:' . $mwsExpr );
+		$res = Http::post( $wgMWSUrl, array( "postData" => $mwsExpr, "timeout" => 60 ) );
+		if ( $res == false ) {
+			if ( function_exists( 'curl_init' ) ) {
 				$handle = curl_init();
 				$options = array(
 					CURLOPT_URL => $wgMWSUrl,
 					CURLOPT_CUSTOMREQUEST => 'POST', // GET POST PUT PATCH DELETE HEAD OPTIONS
 				);
-				//TODO: Figure out how not to write the error in a message and not in top of the output page
-				curl_setopt_array($handle, $options);
-				$details = curl_exec($handle);
+				// TODO: Figure out how not to write the error in a message and not in top of the output page
+				curl_setopt_array( $handle, $options );
+				$details = curl_exec( $handle );
 			} else {
 				$details = "curl is not installed.";
 			}
-			wfDebugLog("MathSearch", "Nothing retreived from $wgMWSUrl. Check if mwsd is running. Error:" .
-					var_export($details, true));
+			wfDebugLog( "MathSearch", "Nothing retreived from $wgMWSUrl. Check if mwsd is running. Error:" .
+					var_export( $details, true ) );
 			return false;
 		}
-		$xres = new SimpleXMLElement($res);
-		if ($wgMathDebug) {
+		$xres = new SimpleXMLElement( $res );
+		if ( $wgMathDebug ) {
 			$out = $this->getOutput();
-			$out->addWikiText('<source lang="xml">' . $res . '</source>');
+			$out->addWikiText( '<source lang="xml">' . $res . '</source>' );
 		}
 		$this->numMathResults = (int) $xres["total"];
-		wfDebugLog("MathSearch", $this->numMathResults . " results retreived from $wgMWSUrl.");
-		if ($this->numMathResults == 0)
+		wfDebugLog( "MathSearch", $this->numMathResults . " results retreived from $wgMWSUrl." );
+		if ( $this->numMathResults == 0 )
 			return true;
 		$this->relevantMathMap = array();
 		$this->mathResults = array();
-		$this->processMathResults($xres);
-		if ($this->numMathResults >= $numProcess) {
-			ini_set('memory_limit', '256M');
-			for ($i = $numProcess; $i <= $this->numMathResults; $i += $numProcess) {
-				$query = str_replace("limitmin=\"0\" ", "limitmin=\"$i\" ", $mwsExpr);
-				$res = Http::post($wgMWSUrl, array("postData" => $query, "timeout" => 60));
-				wfDebugLog('mathsearch', 'MWS query:' . $query);
-				if ($res == false) {
-					wfDebugLog("MathSearch", "Nothing retreived from $wgMWSUrl. check if mwsd is running there");
+		$this->processMathResults( $xres );
+		if ( $this->numMathResults >= $numProcess ) {
+			ini_set( 'memory_limit', '256M' );
+			for ( $i = $numProcess; $i <= $this->numMathResults; $i += $numProcess ) {
+				$query = str_replace( "limitmin=\"0\" ", "limitmin=\"$i\" ", $mwsExpr );
+				$res = Http::post( $wgMWSUrl, array( "postData" => $query, "timeout" => 60 ) );
+				wfDebugLog( 'mathsearch', 'MWS query:' . $query );
+				if ( $res == false ) {
+					wfDebugLog( "MathSearch", "Nothing retreived from $wgMWSUrl. check if mwsd is running there" );
 					return false;
 				}
-				$xres = new SimpleXMLElement($res);
-				$this->processMathResults($xres);
+				$xres = new SimpleXMLElement( $res );
+				$this->processMathResults( $xres );
 			}
 		}
 		return true;
@@ -344,20 +345,20 @@ class SpecialMathSearch extends SpecialPage {
 	/**
 	 * @param unknown $xmlRoot
 	 */
-	function processMathResults($xmlRoot) {
-		foreach ($xmlRoot->children("mws", TRUE) as $page) {
+	function processMathResults( $xmlRoot ) {
+		foreach ( $xmlRoot->children( "mws", TRUE ) as $page ) {
 			$attrs = $page->attributes();
-			$uri = explode("#", $attrs["uri"]);
+			$uri = explode( "#", $attrs["uri"] );
 			$pageID = $uri[0];
-			$AnchorID = substr($uri[1], 4);
+			$AnchorID = substr( $uri[1], 4 );
 			$this->relevantMathMap[$pageID] = true;
 			$substarr = array();
 			// $this->mathResults[(string) $pageID][(string) $AnchorID][]=$page->asXML();
-			foreach ($page->children("mws", TRUE) as $substpair) {
+			foreach ( $page->children( "mws", TRUE ) as $substpair ) {
 				$substattrs = $substpair->attributes();
-				$substarr[] = array("qvar" => (string) $substattrs["qvar"], "xpath" => (string) $substattrs["xpath"]);
+				$substarr[] = array( "qvar" => (string) $substattrs["qvar"], "xpath" => (string) $substattrs["xpath"] );
 			}
-			$this->mathResults[(string) $pageID][(string) $AnchorID][] = array("xpath" => (string) $attrs["xpath"], "mappings" => $substarr); // ,"original"=>$page->asXML()
+			$this->mathResults[(string) $pageID][(string) $AnchorID][] = array( "xpath" => (string) $attrs["xpath"], "mappings" => $substarr ); // ,"original"=>$page->asXML()
 		}
 	}
 
