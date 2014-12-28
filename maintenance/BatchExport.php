04f87350e98bfb215d9f86fb8074ee5cdbfa8d4a
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2014-12-28 22:05:37 +0100

    Add option for admis to export all submissions
    Change-Id: I02950763641628902762c80c6820a95762534b48

---
+++ maintenance/BatchExport.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @ingroup Maintenance
+ */
+
+require_once( __DIR__ . '/../../../maintenance/Maintenance.php' );
+
+class BatchExport extends Maintenance {
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Exports  submissions to a folder. \n Each run is named after the following convention: \n \$userName-\$runName-\$runId.csv";
+		$this->addArg( "dir", "The output directory", true );
+	}
+
+	/**
+	 *
+	 */
+	public function execute() {
+		$dir = $this->getArg( 0 );
+		if ( ! is_dir($dir) ){
+			$this->output("{$dir} is not a directory.\n");
+			exit(1);
+		}
+		$dbr = wfGetDB( DB_SLAVE );
+		//runId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+		//runName VARCHAR(45),
+		//userId INT UNSIGNED,
+		//isDraft TINYINT NOT NULL,
+		$res = $dbr->select( "math_wmc_runs", "*" );
+		//TODO: Implement support for isDraft.
+		foreach ( $res as $row ) {
+			$user = User::newFromId( $row->userId );
+			$username = $user->getName();
+			$runName = preg_replace( "#/#","_", escapeSingleString( $row->runName ));
+			$fn = "$dir/$username-$runName-{$row->runId}.csv";
+			$this->output("Export to file $fn.\n");
+			$fh =  fopen( $fn, 'w' );
+			fwrite( $fh, SpecialMathDownloadResult::run2CSV( $row->runId ) );
+			fclose( $fh );
+		}
+	}
+}
+
+$maintClass = "BatchExport";
+require_once( RUN_MAINTENANCE_IF_MAIN );
