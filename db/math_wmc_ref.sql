8e201118832e6e68cf7cc959fabe48f662afcd11
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2014-12-31 14:06:59 +0100

    Improve layout of reference table
    Change-Id: I8eaa26449b6147ca0a1858561ca2c3fbcd95f6b1

--- db/math_wmc_ref.sql
+++ db/math_wmc_ref.sql
@@ -1,5 +1,9 @@
 CREATE TABLE math_wmc_ref (
     qId INT PRIMARY KEY NOT NULL,
     oldId INT,
-    fId INT
+    fId INT,
+    qVarCount SMALLINT,
+    texQuery TEXT,
+    isDraft TINYINT,
+    math_inputhash VARBINARY(16)
 );
