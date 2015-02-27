c154b4bc1c52a1260fe3df8e0cf21dfe0769d42a
physikerwelt <wiki@physikerwelt.de>
2015-02-27 10:34:41 -0500

    Fix bad foreign key constraints
    Change-Id: I2233ca99c14d61d84237ad3fab356d00c593c7af

--- db/mathrevisionstat.sql
+++ db/mathrevisionstat.sql
@@ -2,11 +2,11 @@
 -- Used by the math search module to analyse the variables in the equations.
 --
 CREATE TABLE /*_*/mathrevisionstat (
-  revstat_revid int(10) NOT NULL,
-  revstat_featureid int(6) NOT NULL,
-  revstat_featurecount int(11) NOT NULL,
+  revstat_revid INT(10) UNSIGNED NOT NULL,
+  revstat_featureid INT(6) NOT NULL,
+  revstat_featurecount INT(11) NOT NULL,
   PRIMARY KEY (revstat_revid,revstat_featureid),
   FOREIGN KEY `revision` ( revstat_revid ) REFERENCES revision( rev_id ),
-  FOREIGN KEY `featureID` ( revstat_featureid ) REFERENCES mathvarstat ( varstat_featurename )
+  FOREIGN KEY `featureID` ( revstat_featureid ) REFERENCES mathvarstat ( varstat_id )
 ) /*$wgDBTableOptions*/;
 
--- db/mathvarstat.sql
+++ db/mathvarstat.sql
@@ -2,7 +2,7 @@
 -- Used by the math search module to analyse the variables in the equations.
 --
 CREATE TABLE /*_*/mathvarstat (
-  varstat_id smallint(6) NOT NULL AUTO_INCREMENT,
+  varstat_id INT(6) NOT NULL AUTO_INCREMENT,
   varstat_featurename varchar(10) NOT NULL,
   varstat_featuretype varchar(10) NOT NULL,
   varstat_featurecount int(11) NOT NULL,
