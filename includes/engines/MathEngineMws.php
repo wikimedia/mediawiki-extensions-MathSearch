c80ffee5bc321b219422e4d620289edaf2d8f763
physikerwelt <wiki@physikerwelt.de>
2015-05-26 22:09:47 +0200

    Ignore malformatted math index entries
    * Some entries in the math index do not follow the
      contention math.$oldId.$forumlaId those formulae
      are ignored
    
    Change-Id: Ie727d45a463c4039eec3c492c35f8173f257623b

--- includes/engines/MathEngineMws.php
+++ includes/engines/MathEngineMws.php
@@ -23,16 +23,29 @@ class MathEngineMws extends MathEngineRest {
 		foreach ( $xmlRoot->children( "mws", TRUE ) as $page ) {
 			$attrs = $page->attributes();
 			$uri = explode( ".", $attrs["uri"] );
-			$revisionID = $uri[1];
-			$AnchorID = $uri[2];
-			$this->relevanceMap[] = $revisionID;
-			$substarr = array();
-			// $this->mathResults[(string) $pageID][(string) $AnchorID][]=$page->asXML();
-			foreach ( $page->children( "mws", TRUE ) as $substpair ) {
-				$substattrs = $substpair->attributes();
-				$substarr[] = array( "qvar" => (string) $substattrs["qvar"], "xpath" => (string) $substattrs["xpath"] );
+			if ( sizeof( $uri ) > 2 ) {
+				$revisionID = $uri[1];
+				$AnchorID = $uri[2];
+				$this->relevanceMap[] = $revisionID;
+				$substarr = array();
+				// $this->mathResults[(string) $pageID][(string) $AnchorID][]=$page->asXML();
+				foreach ( $page->children( "mws", TRUE ) as $substpair ) {
+					$substattrs = $substpair->attributes();
+					$substarr[] =
+						array(
+							"qvar"  => (string)$substattrs["qvar"],
+							"xpath" => (string)$substattrs["xpath"]
+						);
+				}
+				$this->resultSet[(string)$revisionID][(string)$AnchorID][] =
+					array(
+						"xpath"    => (string)$attrs["xpath"],
+						"mappings" => $substarr
+					); // ,"original"=>$page->asXML()
+			} else {
+				LoggerFactory::getInstance( 'MathSeach' )->error( $attrs["uri"] .
+																  ' has an invalid result format.' );
 			}
-			$this->resultSet[(string) $revisionID][(string) $AnchorID][] = array( "xpath" => (string) $attrs["xpath"], "mappings" => $substarr ); // ,"original"=>$page->asXML()
 		}
 		$this->relevanceMap = array_unique( $this->relevanceMap );
 	}
