e787c96dc6cafdde9f0e44f8c16cb12fe4c28a7f
physikerwelt <wiki@physikerwelt.de>
2015-06-09 14:43:29 +0200

    Tolerate user defined formula ids
    Currently only automatically generated formula ids in the form "math.$revisionId.$formulaId" were
    allowed. Now also $revisionID#$userDefinedName is accepted as well.
    
    Change-Id: I7c8a5e3358b7326922034ab4b36fc302b2b11cea

--- includes/engines/MathEngineMws.php
+++ includes/engines/MathEngineMws.php
@@ -22,32 +22,38 @@ class MathEngineMws extends MathEngineRest {
 	function processMathResults( $xmlRoot ) {
 		foreach ( $xmlRoot->children( "mws", TRUE ) as $page ) {
 			$attrs = $page->attributes();
-			$uri = explode( ".", $attrs["uri"] );
-			if ( sizeof( $uri ) > 2 ) {
-				$revisionID = $uri[1];
-				$AnchorID = $uri[2];
-				$this->relevanceMap[] = $revisionID;
-				$substarr = array();
-				// $this->mathResults[(string) $pageID][(string) $AnchorID][]=$page->asXML();
-				foreach ( $page->children( "mws", TRUE ) as $substpair ) {
-					$substattrs = $substpair->attributes();
-					$substarr[] =
-						array(
-							"qvar"  => (string)$substattrs["qvar"],
-							"xpath" => (string)$substattrs["xpath"]
-						);
-				}
-				$this->resultSet[(string)$revisionID][(string)$AnchorID][] =
-					array(
-						"xpath"    => (string)$attrs["xpath"],
-						"mappings" => $substarr
-					); // ,"original"=>$page->asXML()
+			if( strpos( $attrs["uri"], '#' ) ){
+				$uri = explode( "#", $attrs["uri"] );
+				$revisionID = $uri[0];
+				$AnchorID = $uri[1];
 			} else {
-				LoggerFactory::getInstance( 'MathSeach' )->error( $attrs["uri"] .
-																  ' has an invalid result format.' );
+				$uri = explode( ".", $attrs["uri"] );
+				if ( sizeof( $uri ) > 2 ) {
+					$revisionID = $uri[1];
+					$AnchorID = $uri[2];
+				} else {
+					LoggerFactory::getInstance( 'MathSeach' )->error( $attrs["uri"] .
+						' has an invalid result format.' );
+					continue;
+				}
 			}
+			$this->relevanceMap[] = $revisionID;
+			$substarr = array();
+			// $this->mathResults[(string) $pageID][(string) $AnchorID][]=$page->asXML();
+			foreach ( $page->children( "mws", TRUE ) as $substpair ) {
+				$substattrs = $substpair->attributes();
+				$substarr[] =
+					array(
+						"qvar"  => (string)$substattrs["qvar"],
+						"xpath" => (string)$substattrs["xpath"]
+					);
+			}
+			$this->resultSet[(string)$revisionID][(string)$AnchorID][] =
+				array(
+					"xpath"    => (string)$attrs["xpath"],
+					"mappings" => $substarr
+				); // ,"original"=>$page->asXML()
 		}
 		$this->relevanceMap = array_unique( $this->relevanceMap );
 	}
-
 }
