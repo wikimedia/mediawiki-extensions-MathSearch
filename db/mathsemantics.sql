3e8f1ab552859deaaa53183778ac2060048c6893
physikerwelt <wiki@physikerwelt.de>
2015-12-30 18:32:03 +0100

    Increase length of the noun
    combinations of multiple words are allowed now
    
    Change-Id: I20c0ea63375d3891b781c6b106acdc1d218cdc17

--- db/mathsemantics.sql
+++ db/mathsemantics.sql
@@ -2,7 +2,7 @@ CREATE TABLE /*_*/mathsemantics (
   `revision_id` int(10) UNSIGNED NOT NULL,
   `identifier` varchar(20) NOT NULL,
   `evidence` double NOT NULL,
-  `noun` varchar(20) NOT NULL,
+  `noun` varchar(255) NOT NULL,
   `sentence` text NULL,
   KEY `revision_id` (`revision_id`)
 ) /*$wgDBTableOptions*/;
