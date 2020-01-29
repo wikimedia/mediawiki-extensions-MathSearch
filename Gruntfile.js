5dd91e8edf106b88cd5507af9b62ef0ff137319e
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2020-01-29 17:47:15 +0000

    build: Updating composer dependencies
    * jakub-onderka/php-console-highlighter: 0.3.2 → 0.4.0
    * mediawiki/minus-x: 0.3.2 → 1.0.0
    
    Additional changes:
    * Enable eslint caching.
    
    Change-Id: I264e11a2874d7d1668e42cac26871f6a3ce8540d

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,8 @@ module.exports = function ( grunt ) {
 		),
 		eslint: {
 			options: {
-				extensions: [ '.js', '.json' ]
+				extensions: [ '.js', '.json' ],
+				cache: true
 			},
 			all: [
 				'**/*.js{,on}',
