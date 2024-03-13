6e69a286b8539553ea37229ccb894e35879b2b89
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-03-13 14:42:30 +0100

    Only create pages for items without sitelinks
    Change-Id: I89e249d0602b00e27ef6e2e531ddc8a3cb7b15e6

--- includes/Graph/Query.php
+++ includes/Graph/Query.php
@@ -24,7 +24,9 @@ PREFIX wdt: <https://portal.mardi4nfdi.de/prop/direct/>
 PREFIX wd: <https://portal.mardi4nfdi.de/entity/>
 SELECT ?qid WHERE {
     BIND (REPLACE(STR(?item), "^.*/Q([^/]*)$", "$1") as ?qid)
-    ?item wdt:P$wgMathSearchPropertyProfileType wd:${wgMathProfileQIdMap[$type]}
+    ?item wdt:P$wgMathSearchPropertyProfileType wd:${wgMathProfileQIdMap[$type]} .
+    ?item wikibase:sitelinks ?sitelinks .
+    FILTER (?sitelinks < 1 ).
 }
 LIMIT $limit
 OFFSET $offset
