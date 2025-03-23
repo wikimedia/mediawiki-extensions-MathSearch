65a8305472b5cf98abb6b2061eda1fdc10225896
physikerwelt <wiki@physikerwelt.de>
2025-03-23 13:43:13 +0100

    Fix order or assertEquals arguments in testHash
    Change-Id: Idbac322133c4882148026de03bbbed550f14f346

--- tests/phpunit/MathEngineTest.php
+++ tests/phpunit/MathEngineTest.php
@@ -12,7 +12,7 @@ class MathEngineTest extends MediaWikiIntegrationTestCase {
 		$test_hash = 'cd3401b0f0692f2818f217807fa9cc48';
 		$renderer = new MathLaTeXML( $test_tex );
 		$realHash = $renderer->getInputHash();
-		$this->assertEquals( $realHash, $test_hash, 'wrong hash' );
+		$this->assertEquals( $test_hash, $realHash, 'wrong hash' );
 	}
 
 }
