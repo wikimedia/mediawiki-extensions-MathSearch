76f94030e875570f0de58877988da2dbba698b88
physikerwelt <wiki@physikerwelt.de>
2025-05-30 15:00:17 +0200

    Make performance testing script independet of the os
    Change-Id: I2493f8b5890b2d2cfe2585d335c75ee9d51f7089

--- maintenance/performance.sh
+++ maintenance/performance.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 START=0
-END=$(sysctl -n hw.physicalcpu) #1
-for ((i=START; i<END; i++))
-do
-   echo "Starting process: $i"
-   # php ./MathPerformance.php benchmark $END $i --table=statistics --input=tex --hash=hash -v > log"$i".out &
-   php ./MathPerformance.php benchmark "$END $i" &
+# Determine number of available logical CPUs (can be overridden by argument or environment variable)
+if [ -n "$1" ]; then
+    END="$1"
+elif [ -n "$END" ]; then
+    END="$END"
+else
+    END=$(getconf _NPROCESSORS_ONLN 2>/dev/null)
+    if ! [[ "$END" =~ ^[0-9]+$ ]] || [ "$END" -lt 1 ]; then
+        END=1
+    fi
+fi
+echo "Using $END parallel processes."
+
+for ((i=START; i<END; i++)); do
+    echo "Starting process: $i"
+    (
+	   # php ./MathPerformance.php benchmark $END $i --table=statistics --input=tex --hash=hash -v > log"$i".out &
+	   php ./MathPerformance.php benchmark $END $i
+       echo "Process $i finished."
+    ) &
 done
+
+wait
+echo "All processes have completed."
