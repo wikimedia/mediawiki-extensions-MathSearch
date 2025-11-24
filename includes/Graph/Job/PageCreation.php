f6b504a52dd0f6cf2495d965f8e7e1eb9d8705c0
physikerwelt <wiki@physikerwelt.de>
2025-11-24 14:17:33 +0100

    Use actual titles for profile pages
    Instead of using generic names for profile pages such
    as Person:42 use descriptive profiles in the
    main namespace such as Douglas Adams.
    
    cf https://github.com/MaRDI4NFDI/portal-k8s/issues/26
    
    Change-Id: Iab49652b67871c67442aeae3ae36c66d39d36c53

--- includes/Graph/Job/PageCreation.php
+++ includes/Graph/Job/PageCreation.php
@@ -4,8 +4,12 @@ namespace MediaWiki\Extension\MathSearch\Graph\Job;
 use ContentHandler;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
+use MediaWiki\Title\TitleParser;
+use RuntimeException;
 use Throwable;
+use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Services\Statement\V4GuidGenerator;
 use Wikibase\DataModel\SiteLink;
 use Wikibase\Repo\WikibaseRepo;
 
@@ -23,26 +27,81 @@ class PageCreation extends GraphJob {
 		foreach ( $this->params['rows'] as $qid ) {
 			try {
 				self::getLog()->info( "Creating page $qid." );
-				$title = Title::newFromText( $this->params['prefix'] . ':' . $qid );
-				$pageContent = ContentHandler::makeContent(
-					'{{' . $this->params['prefix'] . '}}', $title );
-				$pageFactory->newFromTitle( $title )
-					->doUserEditContent( $pageContent, $user,
-						'Created automatically from ' . $this->params['jobname'] );
 				$item = $lookup->getEntity( new ItemId( $qid ) );
-				if ( $this->params['overwrite'] ?? false ) {
-					$item->removeSiteLink( 'mardi' );
+				if ( $item === null ) {
+					self::getLog()->error( "Item Q$qid not found." );
+					continue;
 				}
-				$siteLink = new SiteLink( 'mardi', $title->getPrefixedText() );
+				$newTitle = $this->makeTitle( $item );
+				if ( $item->hasLinkToSite( 'mardi' ) ) {
+					self::getLog()->info( "Page for $qid already exists." );
+					if ( $item->getSiteLink( 'mardi' )->getPageName() === $newTitle->getPrefixedText() ) {
+						continue;
+					}
+					self::getLog()->info( "Moving existing page to " . $newTitle->getText() . "." );
+					$status = MediaWikiServices::getInstance()->getMovePageFactory()->newMovePage(
+						Title::newFromText( $item->getSiteLink( 'mardi' )->getPageName() ),
+						$this->makeTitle( $item )
+					)->move( $user,
+						'Move profile page according to new naming schema. Job ' . $this->params['jobname'] );
+					if ( !$status->isOK() ) {
+						self::getLog()->error( "Could not move page for $qid: " . $status->getMessage()->text() );
+						continue;
+					}
+					$item->removeSiteLink( 'mardi' );
+				} else {
+					self::getLog()->info( "Creating new page for $qid." );
+					$pageContent = ContentHandler::makeContent(
+						'{{' . $this->params['prefix'] . '}}', $newTitle );
+					$pageFactory->newFromTitle( $newTitle )->doUserEditContent( $pageContent, $user,
+						'Created automatically from ' . $this->params['jobname'] );
+				}
+				$siteLink = new SiteLink( 'mardi', $newTitle->getPrefixedText() );
 				$item->addSiteLink( $siteLink );
 				self::getLog()->info( "Linking page $qid." );
 				$store->saveEntity( $item, "Added link to MaRDI item.", $user, EDIT_FORCE_BOT );
 			} catch ( Throwable $ex ) {
-				self::getLog()->error( "Skip page processing page Q$qid.", [ $ex ] );
+				self::getLog()->error( "Skip page processing page $qid.", [ $ex ] );
 			}
 		}
 
 		return true;
+	}
+
+	private function makeTitle( Item $item ): Title {
+		$label = $item->getLabels()->hasTermForLanguage( 'en' ) ?
+			$item->getLabels()->getByLanguage( 'en' )->getText() :
+			'';
+		$description = $item->getDescriptions()->hasTermForLanguage( 'en' ) ?
+			$item->getDescriptions()->getByLanguage( 'en' )->getText() :
+			'';
+		$id = $item->getId()->getSerialization();
+		$titleOptions = [
+			$label ?: $description,
+			$label . '_(' . $description . ')',
+			$label . '_' . $id,
+			$this->params['prefix'] . ':' . str_replace( 'Q', '', $id ),
+			( new V4GuidGenerator() )->newGuid()
+		];
+
+		foreach ( $titleOptions as $titleOption ) {
+			$t = $this->checkTitle( $titleOption );
+			if ( $t !== null ) {
+				return $t;
+			}
+		}
+		throw new RuntimeException( "Could not create unique title for item " . $id );
+	}
+
+	private function checkTitle( string $title ): ?Title {
+		if ( preg_match( TitleParser::getTitleInvalidRegex(), $title ) === 1 ) {
+			self::getLog()->info( "Title for $title contains invalid chars.", [ $title ] );
+		}
+		$t = Title::newFromText( $title );
+		if ( $t === null || $t->exists() ) {
+			return null;
+		}
+		return $t;
 	}
 
 }
