e4c7daeab90c250b042a48af6b4546795a89cb06
physikerwelt <wiki@physikerwelt.de>
2026-07-07 17:19:54 +0200

    Drop FK constraint from math_wmc_assessed_formula table
    Change-Id: I190a3812a1126ee3b6f21a5554ff1459456a229c

--- db/wmc/persistent/math_wmc_assessed_formula.sql
+++ db/wmc/persistent/math_wmc_assessed_formula.sql
@@ -5,6 +5,5 @@ CREATE TABLE math_wmc_assessed_formula (
   assessor INT(10) UNSIGNED,
   FOREIGN KEY ( qId ) REFERENCES math_wmc_ref ( qId ) ON DELETE CASCADE,
   FOREIGN KEY ( assessor ) REFERENCES user ( user_id ) ON DELETE SET NULL,
-  FOREIGN KEY ( inputhash ) REFERENCES mathlatexml ( math_inputhash ) ON DELETE CASCADE,
   UNIQUE KEY ( qId, inputhash, assessor)
 );
